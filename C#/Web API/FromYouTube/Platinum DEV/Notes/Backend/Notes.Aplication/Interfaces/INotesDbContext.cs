617ed7619a5448236d62aba16d692d3316293221
fantom3527 <62473392+fantom3527@users.noreply.github.com>
2023-07-31 16:56:05 +0700

    Создание архитектуры.
    * Добавлен Entity Framework.
    * Добавлен Automapper.

---
+++ C#/Web API/FromYouTube/Platinum DEV/Notes/Backend/Notes.Aplication/Interfaces/INotesDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Notes.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Notes.Aplication.Interfaces
+{
+    public interface INotesDbContext
+    {
+        DbSet<Note> Notes { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
